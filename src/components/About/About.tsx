caf6e5f772b27ec6e42b8b1efb491f9192131c99
JasonChen47 <42682065+JasonChen47@users.noreply.github.com>
2022-01-30 08:57:36 -0500

    edited main about text

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -16,22 +16,22 @@ export function About(){
     <Container id="sobre">
       <div className="about-text">
         <ScrollAnimation animateIn="fadeInLeft">
-          <h2>Sobre mim</h2>
+          <h2>About me</h2>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000}>
-          <p>Me chamo João Túlio, sou técnico em informática com uma grande paixão por programação. Sou apaixonado por entregar soluções que agregam na vida das pessoas e ao mesmo tempo me desafiam. Aprimorei minhas habilidades como desenvolvedor Front-End e Back-End</p>
+          <p>My name is Jason, I have a Bachelors of Arts in Biology, and I am working towards a Masters in Biomedical Engineering. I like to work on ideas, apps, devices, and projects that reduce suffering in the world in any way. </p>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInLeft" delay={0.4 * 1000} style={{marginTop: "2rem", marginBottom: "2rem"}}>
 
-          <p>Desenvolvo sites e aplicações utilizando HTML, CSS, e JavaScript. Tenho familiaridade em desenvolver layouts que me fornecem. Estou sempre me aprimorando a cada projeto que colocam em minhas mãos.</p>
+          <p>I prototype with circuits, CAD, and printing methods. My lab research has been in breast cancer chemotherapy and machine learning.</p>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInLeft" delay={0.6 * 1000}>
 
-          <p>Sou uma pessoa dedicada que corre atrás para realizar seus sonhos, trabalhador e orientado a resultados, sempre busco alcançar minha melhor versão.</p>
+          <p>I am a friendly and laid-back person who gets very passionate when working on projects that can quickly heal, help, or inspire others.</p>
         </ScrollAnimation>
 
         <ScrollAnimation animateIn="fadeInLeft" delay={0.7 * 1000}>
-          <h3>Aqui estão as minhas principais habilidades:</h3>
+          <h3>Here are some coding languages I use:</h3>
         </ScrollAnimation>
         <div className="hard-skills">
 
