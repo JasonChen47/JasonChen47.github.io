a3c8b4bbc9ac1775b9ccb62ed4c43d1f1d7753e2
JasonChen47 <42682065+JasonChen47@users.noreply.github.com>
2022-02-08 09:57:51 -0500

    rick roll success

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import Konami, { useKonami } from 'react-konami-code'
 import { useEffect } from 'react'
 import { Footer } from './components/Footer/Footer'
 import { Header } from './components/Header/Header'
 import { Main } from './components/Main/Main'
 import { GlobalStyle } from './styles/global'
 import 'react-toastify/dist/ReactToastify.css'
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route, 
+  Link
+} from "react-router-dom";
+
+const easterEgg = () => {
+  alert('Hey, you typed the Konami Code!');
+  window.open('https://www.youtube.com/watch?v=iik25wqIuFo', '_blank');
+}
+
 function App() {
+  useKonami(easterEgg);
   return (
     <>
       <GlobalStyle></GlobalStyle>
